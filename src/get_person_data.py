6ca5b73741d015e42840b926281e9aad7045a203
vladsiculan <vladsiculan@gmail.com>
2024-11-18 17:57:51 +0200

    initial commit

---
+++ src/get_person_data.py
@@ -0,0 +1,20 @@
+"""Get person data"""
+
+DATA = {
+     111: {
+         "name": "Dan",
+         "age": 20
+     },
+     112: {
+         "name": "Maria",
+         "age": 33
+     },
+}
+
+def get_name_and_age(person_id):
+    """Return person name and age"""
+    return DATA[person_id]
+
+if __name__ == "__main__":
+    person = get_name_and_age(112)
+    print(person)
