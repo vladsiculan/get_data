4bcfc913e8cf138656cc422c17558f343c8334f1
vladsiculan <vladsiculan@gmail.com>
2024-11-18 18:04:18 +0200

    added exception handling

--- src/get_person_data.py
+++ src/get_person_data.py
@@ -15,6 +15,10 @@ def get_name_and_age(person_id):
     """Return person name and age"""
     return DATA[person_id]
 
+
 if __name__ == "__main__":
-    person = get_name_and_age(112)
-    print(person)
+    try:
+        person = get_name_and_age(112)
+        print(person)
+    except KeyError:
+        print("Not a good user id")
