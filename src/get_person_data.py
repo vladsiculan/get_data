0b2b9fe6b80c674ad9fb4079cec19bb604590a54
vladsiculan <vladsiculan@gmail.com>
2024-11-18 18:15:32 +0200

    developed save_data() function

--- src/get_person_data.py
+++ src/get_person_data.py
@@ -16,6 +16,12 @@ def get_name_and_age(person_id):
     return DATA[person_id]
 
 
+def save_data(file, person_data):
+    """Save person data to file"""
+    with open(file, "w") as fw:
+        fw.write(person_data)
+
+
 if __name__ == "__main__":
     try:
         person = get_name_and_age(112)
